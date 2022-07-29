94de176216db55002529717791bca079b3341746
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-07-29 04:54:55 +0100

    showanswer function added

--- puzzle-lottery/js/trivia.js
+++ puzzle-lottery/js/trivia.js
@@ -15,10 +15,15 @@ let trackTwo = document.querySelectorAll(".question-track-2");
 let trackThree = document.querySelectorAll(".question-track-3");
 let trackFour = document.querySelectorAll(".question-track-4");
 let trackFive = document.querySelectorAll(".question-track-5");
+answerOne = "Madrid";
+answerTwo = "Black Widow";
+answerThree = "Femur";
+answerFour = "Netherlands";
+answerFive = "1995";
 
 window.addEventListener("load", () => {
     showSlides(slideIndex);
-    questionTimer = setInterval(function() { nextQuestion(1)}, 13000);
+    questionTimer = setInterval(function() { showAnswer(slideIndex)}, 13000);
 })
 
 function showSlides() {
@@ -37,21 +42,79 @@ function showSlides() {
     }
 }
 
+function showAnswer(n) {
+    console.log(`slideIndex is ${slideIndex}`)
+    let allOptions = document.querySelectorAll(`.q${n}-options`);
+    allOptions.forEach(option => {
+        option.checked = true;
+        option.parentElement.classList.replace("dark-grey-border", "red-border");
+        option.previousElementSibling.classList.replace("text-grey-2", "text-red");
+        option.style.backgroundImage = "url('img/redx-1.png')";
+    })
+    if (n == 1) {
+        let correctOption = document.querySelector(`.q${n}-options[value='${answerOne}']`);
+        correctOption.parentElement.classList.replace("red-border", "green-border");
+        correctOption.previousElementSibling.classList.replace("text-red", "text-green");
+        correctOption.checked = true;
+        correctOption.style.backgroundImage = "url('img/green-tick.png')";
+        trackOne.forEach(track => {
+            track.classList.replace("bg-white", "bg-red");
+            track.classList.replace("bg-neutral-1", "bg-red");
+        })
+    } else if (n == 2) {
+        let correctOption = document.querySelector(`.q${n}-options[value='${answerTwo}']`);
+        correctOption.parentElement.classList.replace("red-border", "green-border");
+        correctOption.previousElementSibling.classList.replace("text-red", "text-green");
+        correctOption.checked = true;
+        correctOption.style.backgroundImage = "url('img/green-tick.png')";
+        trackTwo.forEach(track => {
+            track.classList.replace("bg-white", "bg-red");
+            track.classList.replace("bg-neutral-1", "bg-red");
+        })
+    } else if (n == 3) {
+        let correctOption = document.querySelector(`.q${n}-options[value='${answerThree}']`);
+        correctOption.parentElement.classList.replace("red-border", "green-border");
+        correctOption.previousElementSibling.classList.replace("text-red", "text-green");
+        correctOption.checked = true;
+        correctOption.style.backgroundImage = "url('img/green-tick.png')";
+        trackThree.forEach(track => {
+            track.classList.replace("bg-white", "bg-red");
+            track.classList.replace("bg-neutral-1", "bg-red");
+        })
+    } else if (n == 4) {
+        let correctOption = document.querySelector(`.q${n}-options[value='${answerFour}']`);
+        correctOption.parentElement.classList.replace("red-border", "green-border");
+        correctOption.previousElementSibling.classList.replace("text-red", "text-green");
+        correctOption.checked = true;
+        correctOption.style.backgroundImage = "url('img/green-tick.png')";
+        trackFour.forEach(track => {
+            track.classList.replace("bg-white", "bg-red");
+            track.classList.replace("bg-neutral-1", "bg-red");
+        })
+    } else if (n == 5) {
+        let correctOption = document.querySelector(`.q${n}-options[value='${answerFive}']`);
+        correctOption.parentElement.classList.replace("red-border", "green-border");
+        correctOption.previousElementSibling.classList.replace("text-red", "text-green");
+        correctOption.checked = true;
+        correctOption.style.backgroundImage = "url('img/green-tick.png')";
+        trackFive.forEach(track => {
+            track.classList.replace("bg-white", "bg-red");
+            track.classList.replace("bg-neutral-1", "bg-red");
+        })
+    }
+    clearInterval(questionTimer);
+    questionTimer = setInterval(function() { nextQuestion(2)}, 2000);
+}
+
 function nextQuestion(n) {
     clearInterval(questionTimer);
     
     if (slideIndex != slides.length + 1) {
         showSlides(slideIndex += 1);
 
-        questionTimer = setInterval(function() { nextQuestion(n + 1)}, 13000);
+        questionTimer = setInterval(function() { showAnswer(slideIndex)}, 13000);
     }
 }
-
-answerOne = "Madrid";
-answerTwo = "Black Widow";
-answerThree = "Femur";
-answerFour = "Netherlands";
-answerFive = "1995";
 
 let radios = document.querySelectorAll('input[type="checkbox"]');
 
